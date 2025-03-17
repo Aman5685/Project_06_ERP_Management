e8089026dc6ab26343cca0b9f2ffe04493634461
Aman5685 <amanyadav5685@gmail.com>
2025-03-17 23:08:09 +0530

    V3 Done

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+const nextConfig = {
+    reactStrictMode: true, // Recommended for React apps
+    experimental: {
+      // Add any experimental features if needed
+    },
+  };
+  
+  export default nextConfig;
+  
